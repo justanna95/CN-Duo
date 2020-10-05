3e4ff82de5ff72cefc0a141d7001f4e0d86701ee
Anna-Maree Syme <as6783@gadi-login-01.gadi.nci.org.au>
2020-10-05 13:06:06 +1100

    updating 3 state

--- check-el.py
+++ check-el.py
@@ -9,9 +9,12 @@ import pandas as pd
 
 
 df = pd.read_csv('chk.txt', sep = '\s+', usecols=[2,6,9,10], names= [ "J", "delta", "el", "v"])
-df.head()
+df["absdiff"] = abs(df["delta"])
 pd.set_option('display.max_rows', df.shape[0]+1)
 
+print("abs")
+print(df.groupby("el").absdiff.mean())
+print("mad")
 print(df.groupby("el").delta.mad())
 
 el = [1,2,3]
